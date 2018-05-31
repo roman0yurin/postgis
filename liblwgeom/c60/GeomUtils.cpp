149bc3eae74c6f6e7278c1e437d23a6c68cd0209
roman <gooood85@yandex.ru>
2018-05-31 23:09:29 +0500

    Добавлено и доработано несколько функций для анализа 3D геометрии.

--- liblwgeom/c60/GeomUtils.cpp
+++ liblwgeom/c60/GeomUtils.cpp
@@ -5,6 +5,7 @@
 
 #include "GeomUtils.hpp"
 #include <assert.h>
+#include <liblwgeom.h>
 
 extern "C"{
 
@@ -37,6 +38,31 @@ extern "C"{
 	Datum c60_getGeomByteLen(PG_FUNCTION_ARGS){
 		GSERIALIZED *geom = (GSERIALIZED*)PG_DETOAST_DATUM(PG_GETARG_POINTER(0));
 		PG_RETURN_INT32(geom->size);
+	}
+
+	/**
+	* Быстрое извлечение 3D центроида геометри (без полного чтения).
+    * Принимает геометри возвращает 3д точку.
+	**/
+	PG_FUNCTION_INFO_V1(c60_getMbrCentroid3D);
+	Datum c60_getMbrCentroid3D(PG_FUNCTION_ARGS){
+		GSERIALIZED *geom = (GSERIALIZED*)PG_DETOAST_DATUM(PG_GETARG_POINTER(0));
+		GBOX bbox;
+		if ( gserialized_read_gbox_p(geom, &bbox) == LW_SUCCESS ){
+			int32_t srid = gserialized_get_srid(geom);
+			LWPOINT *center = lwpoint_make3dz(
+					srid,
+					(bbox.xmin + bbox.xmax)/2,
+					(bbox.ymin + bbox.ymax)/2,
+					(bbox.zmin + bbox.zmax)/2
+			);
+			GSERIALIZED *result = geometry_serialize(lwpoint_as_lwgeom(center));
+			lwpoint_free(center);
+			PG_RETURN_POINTER(result);
+		}else{
+
+			PG_RETURN_NULL();
+		}
 	}
 
 }
--- liblwgeom/lwgeom.c
+++ liblwgeom/lwgeom.c
@@ -1274,6 +1274,9 @@ uint32_t lwgeom_count_vertices(const LWGEOM *geom)
 	case COLLECTIONTYPE:
 		result = lwcollection_count_vertices((LWCOLLECTION *)geom);
 		break;
+	case REF3D_TYPE:
+		result = 8;
+		break;
 	default:
 		lwerror("%s: unsupported input geometry type: %s",
 		        __func__, lwtype_name(geom->type));
